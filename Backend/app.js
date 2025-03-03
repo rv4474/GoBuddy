55d95e9e71481864f4926cb7f27cf7c2085c338d
Rajan verma <rv4474990@gmail.com>
2025-03-03 11:21:22 +0530

    Commit

--- Backend/app.js
+++ Backend/app.js
@@ -3,8 +3,10 @@ dotenv.config();
 const cors=require('cors')
 const express=require('express');
 const app=express();
+const connectToDb= require('./db/db');
 
 
+connectToDb();
 app.use(cors());
 app.get('/',(req,res)=>{
     res.send("Hello World");
